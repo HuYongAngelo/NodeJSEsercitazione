c506b0aaecb0dc2e137e9ccccfb644dbdb1b0d75
Hu Yong An <hu.yongan@osdbsesto>
2022-02-24 11:20:51 +0100

    Bootstrap integrated

--- back/index.js
+++ back/index.js
@@ -16,14 +16,18 @@ apiServer.get("/api/login", (req, res) => {
   fs.readFile("users.json", (err, data) => {
     if (err) {
       console.log(err);
-      res.status(500).json({message: "errore generico"});
+      res.status(500).json({ message: "errore generico" });
     } else {
+      var login = false;
       var users = JSON.parse(data);
       users.forEach((a) => {
         if (a.mail === req.query.mail && a.password === req.query.password) {
           res.status(200).json({ message: "login effettuato" });
-        } else res.status(400).json({ message: "login fallito" });
+          login = true;
+          return;
+        }
       });
+      if (!login) res.status(400).json({ message: "login failed" });
     }
   });
 });
